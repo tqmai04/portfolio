1ac3d08a9f1ce1f65828bf5d544e1849a1c64931
Mai <tqmai04@gmail.com>
2018-04-02 01:40:18 -0500

    adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp'),
-watch = require("gulp-watch");
+watch = require("gulp-watch"), 
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssvars = require("postcss-simple-vars"),
+nested = require("postcss-nested"),
+cssImport = require("postcss-import");
 
 gulp.task('default', function() {
     console.log("Lmao");
+});
+
+gulp.task("styles", function() {
+    return gulp.src("./assets/styles/style.css")
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest("./assets/temp/styles/styles.css"));
 });
 
 gulp.task('watch', function() {
