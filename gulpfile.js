fb465e9cbc4856783c7e4d94dc116da9be4eec30
Mai <tqmai04@gmail.com>
2018-04-14 21:24:42 -0500

    added responsive images

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,4 @@
-var gulp = require('gulp'),
-watch = require("gulp-watch"), 
-postcss = require("gulp-postcss"),
-autoprefixer = require("autoprefixer"),
-cssvars = require("postcss-simple-vars"),
-nested = require("postcss-nested"),
-cssImport = require("postcss-import");
-
-gulp.task('default', function() {
-    gulp.start("styles");
-});
-
-gulp.task("styles", function() {
-    return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest("./app/temp/styles/"));
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('watch', function() {
-    watch("./index.html", function() {
-        console.log("Index.html has been changed!");
-});
-});
