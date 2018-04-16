6d8451d9e2e67a0488bb8dccfb1396a28dafdba6
Mai <tqmai04@gmail.com>
2018-04-16 04:20:58 -0500

    added projects

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+
+    constructor() {
+        this.itemsToReveal = $(".item");
+        this.hideInitially();
+        this.createWaypoints();
+    }
+
+    hideInitially() {
+        this.itemsToReveal.addClass("reveal-item");
+      }
+    
+      createWaypoints() {
+        this.itemsToReveal.each(function() {
+          var currentItem = this;
+          new Waypoint({
+            element: currentItem,
+            handler: function() {
+              $(currentItem).addClass("reveal-item--visible");
+            },
+            offset: "85%"
+          });
+        });
+      }
+    }
+
+export default RevealOnScroll;
